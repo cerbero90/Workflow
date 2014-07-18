e21e4299c141fc9614f903fcae65fd576a55106a
Andrea Marco Sartori <22cerbero22@gmail.com>
2014-07-18 02:04:42 +0200

    Added codeception

---
+++ tests/_bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+// This is global bootstrap for autoloading 
+include __DIR__.'/../../../../vendor/autoload.php';
+$app = require_once __DIR__.'/../../../../bootstrap/start.php';
+$app->boot();
