9bdc831aee819398ccf9008a9f82f2b9b86f815c
Andrea Marco Sartori <22cerbero22@gmail.com>
2014-07-19 02:18:47 +0200

    Created the workflow command

---
+++ src/Cerbero/Workflow/WorkflowCommand.php
@@ -0,0 +1,68 @@
+<?php namespace Cerbero\Workflow;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class WorkflowCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'workflow';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Speed up the workflow to add new features.';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+		//
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return array(
+			array('name', InputArgument::REQUIRED, 'The name of the workflow.'),
+		);
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return array(
+			array('folder', '-f', InputOption::VALUE_OPTIONAL, 'The folder to place files in.', null),
+			array('namespace', '-n', InputOption::VALUE_OPTIONAL, 'The workflow namespace.', null),
+		);
+	}
+
+}
