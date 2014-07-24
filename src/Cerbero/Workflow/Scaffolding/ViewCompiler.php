a9574b0fbe073feaa6dc93423b78f74d349dcbf4
Andrea Marco Sartori <22cerbero22@gmail.com>
2014-07-24 15:44:02 +0200

    Added methods to compile nested templates

--- src/Cerbero/Workflow/Scaffolding/ViewCompiler.php
+++ src/Cerbero/Workflow/Scaffolding/ViewCompiler.php
@@ -77,11 +77,48 @@ class ViewCompiler implements CompilerInterface
 	 */
 	protected function templatePopulatedBy(Workflow $workflow, $content)
 	{
+		$content = $this->nestTemplates($workflow, $content);
+
 		return preg_replace_callback('/\$([a-z]+)\$/', function($matches) use($workflow)
 		{
 			return $workflow->{$matches[1]};
 
 		}, $content);
+	}
+
+	/**
+	 * Put one template inside another.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	Cerbero\Workflow\WorkflowDataTransfer	$workflow
+	 * @param	string	$content
+	 * @return	string
+	 */
+	protected function nestTemplates(Workflow $workflow, $content)
+	{
+		for($i = 0; $i < count($decos = $workflow->decorators); $i++)
+		{
+			$bind = $this->compileBind($decos[$i], $i + 1);
+
+			$content = str_replace('#bind#', $bind, $content);
+		}
+		return str_replace('#bind#', '$namespace$\$name$', $content);
+	}
+
+	/**
+	 * Compile a bind template.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string	$decorator
+	 * @return	string
+	 */
+	protected function compileBind($decorator, $tabs)
+	{
+		$bind = $this->getContent('bind');
+
+		$tab = str_repeat("\t", $tabs);
+
+		return str_replace(['$decorator$', '$tab$'], [$decorator, $tab], $bind);
 	}
 
 }
