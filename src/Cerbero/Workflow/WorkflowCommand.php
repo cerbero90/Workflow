6198d23de899c744c2b3323f1107632d5f48daa4
Andrea Marco Sartori <22cerbero22@gmail.com>
2014-07-24 23:35:04 +0200

    Removed options

--- src/Cerbero/Workflow/WorkflowCommand.php
+++ src/Cerbero/Workflow/WorkflowCommand.php
@@ -4,7 +4,6 @@ use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Cerbero\Workflow\Scaffolding\GeneratorInterface as Scaffolding;
-use Illuminate\Support\Facades\Config;
 
 class WorkflowCommand extends Command {
 
@@ -88,10 +87,7 @@ class WorkflowCommand extends Command {
 	 */
 	protected function getOptions()
 	{
-		return array(
-			array('folder', null, InputOption::VALUE_OPTIONAL, 'The folder to place files.', Config::get('workflow::folder')),
-			array('namespace', null, InputOption::VALUE_OPTIONAL, 'The workflow namespace.', Config::get('workflow::namespace')),
-		);
+		return array();
 	}
 
 }
