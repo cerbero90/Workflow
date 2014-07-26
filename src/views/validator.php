807bcd9fd5d77091f5b4d26ec8b59f3831a593c3
Andrea Marco Sartori <22cerbero22@gmail.com>
2014-07-26 21:24:07 +0200

    Created template for validators

---
+++ src/views/validator.php
@@ -0,0 +1,52 @@
+<?php namespace $namespace$\Decorators;
+
+use $namespace$\$name$Interface;
+use Cerbero\Workflow\Validation\AbstractValidator;
+
+/**
+ * Decorator of the $lowername$ workflow.
+ *
+ */
+class $decorator$ extends AbstractValidator implements $name$Interface
+{
+
+	/**
+	 * @var		$name$Interface	$$lowername$	$name$ implementation.
+	 */
+	protected $$lowername$;
+	
+	/**
+	 * Set the dependencies.
+	 *
+	 * @param	$name$Interface	$$lowername$
+	 * @return	void
+	 */
+	public function __construct($name$Interface $$lowername$)
+	{
+		$this->$lowername$ = $$lowername$;
+	}
+
+	/**
+	 * Trigger the $lowername$ workflow.
+	 *
+	 * @param	$data
+	 */
+	public function $method$($data = null)
+	{
+		$this->validate($data);
+
+		$this->$lowername$->$method$($data);
+	}
+
+	/**
+	 * Retrieve the rules to apply.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @return	array
+	 */
+	protected function getRules()
+	{
+		return [];
+	}
+
+}
