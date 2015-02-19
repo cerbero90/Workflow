f1b60cb20dffc15c7cb43c35ec65049c4fc342ef
Andrea Marco Sartori <22cerbero22@gmail.com>
2015-02-20 00:44:46 +0100

    Create trait

---
+++ src/Console/Commands/DeleteIfForcedTrait.php
@@ -0,0 +1,29 @@
+<?php namespace Cerbero\Workflow\Console\Commands;
+
+/**
+ * Trait to delete files if forced.
+ *
+ * @author	Andrea Marco Sartori
+ */
+trait DeleteIfForcedTrait {
+
+	/**
+	 * Delete the given files if force is set.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @return	void
+	 */
+	protected function deleteIfForced(array $files)
+	{
+		if( ! $this->option('force')) return;
+
+		foreach ($files as $file)
+		{
+			if($this->files->exists($path = $this->getPath($file)))
+			{
+				$this->files->delete($path);
+			}
+		}
+	}
+
+}
